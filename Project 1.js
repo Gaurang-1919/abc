c117bbd59b069dd3ffd274c6166a726718090fb7
Gaurang-1919 <gaurang.ug24@nsut.ac.in>
2025-10-05 17:00:03 +0530

    hello

---
+++ Project 1.js
@@ -0,0 +1,62 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+  });
+});
+
+
+
+
+// by using switch case
+
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+buttons.forEach((button) => {
+  console.log(button)
+  button.addEventListener("click", (e) => {
+    // if (e.target.id === "grey") {
+    //   body.style.backgroundColor = e.target.id
+    // }
+
+    let x = e.target.id
+    console.log(x)
+
+    switch (x) {
+      case "grey":
+        body.style.backgroundColor = e.target.id
+        break;
+      case "white":
+        body.style.backgroundColor = e.target.id
+        break;
+      case "blue":
+        body.style.backgroundColor = e.target.id
+        break;
+      case "yellow":
+        body.style.backgroundColor = e.target.id
+        break;
+      
+      default:
+        console.log("Click on the color box")
+        break;
+    }
+  })
+})
